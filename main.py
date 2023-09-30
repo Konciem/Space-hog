b4165411be915e27005bb3768f58709bc2d06875
Konrad Ciemała <konradciemala@Konrads-MacBook-Air.local>
2023-09-30 20:49:05 +0200

    Add new print

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                print(f'dupa')
                 break
 
         keys = pygame.key.get_pressed()
